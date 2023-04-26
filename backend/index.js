70dc813a385abe2046757ad90017c153ef21c7c7
Lenis45 <odisei.kolesnikov@gmail.com>
2023-04-26 17:32:55 +0300

    frontend, chats, css

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,16 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
-});
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            {username: username, secret: username, first_name: username},
+            {headers: {"private-key": "153d7a99-96fa-464b-b251-50ea44f872b1"}}
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+  });
 
 app.listen(3001);
