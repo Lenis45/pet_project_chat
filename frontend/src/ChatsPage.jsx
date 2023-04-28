ec58b9b564f7502b42edfc58db487e4405d6c0ae
Lenis45 <odisei.kolesnikov@gmail.com>
2023-04-28 23:47:14 +0300

    ChatPage, end

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,12 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(' 25d68175-d52c-4031-89a6-4a4bf6215704', props.user.username, props.user.secret);
+    return (
+    <div style={{ height: '100vh' }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps}  style={{ height: '100%'}}/>
+    </div>
+    )
 }
 export default ChatsPage;
